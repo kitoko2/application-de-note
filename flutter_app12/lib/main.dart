d6b28cea7733d249c468994309febc125f2c9985
kitoko2 <dogbo804@gmail.com>
2021-05-03 21:37:49 +0000

    new sauvegarde

--- flutter_app12/lib/main.dart
+++ flutter_app12/lib/main.dart
@@ -26,7 +26,38 @@ class MyHomme extends StatefulWidget {
   _MyHommeState createState() => _MyHommeState();
 }
 
+List colora = [
+  Color.fromRGBO(100, 12, 5, 0.4),
+  Color.fromRGBO(10, 120, 5, 0.4),
+];
+List colorb = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+];
+
 class _MyHommeState extends State<MyHomme> {
+  var gv = 1;
+  List<Radio> f() {
+    List<Widget> l = [];
+    for (var i = 0; i < 2; i++) {
+      Row row = Row(
+        children: [
+          Radio(
+              value: i,
+              groupValue: gv,
+              onChanged: (e) {
+                setState(() {
+                  gv = e;
+                });
+              })
+        ],
+      );
+      l.add(row);
+    }
+    return l;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,89 +102,99 @@ class _MyHommeState extends State<MyHomme> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     List mesNotes = snapshot.data;
-                    return GridView.builder(
-                      itemCount: mesNotes.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 15,
-                        crossAxisSpacing: 20,
-                      ),
-                      itemBuilder: (BuildContext context, i) {
-                        return Stack(
-                          alignment: Alignment.topRight,
-                          children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width / 2,
-                              height: 200,
-                              decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    width: double.infinity,
-                                    height: 40,
-                                    decoration: BoxDecoration(
-                                      color: Color.fromRGBO(12, 23, 2, 0.5),
-                                      borderRadius: BorderRadius.vertical(
-                                        top: Radius.circular(10),
+                    return Scrollbar(
+                      //juste pour la scrollbar
+                      child: GridView.builder(
+                        itemCount: mesNotes.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 15,
+                          crossAxisSpacing: 20,
+                        ),
+                        itemBuilder: (BuildContext context, i) {
+                          return Stack(
+                            alignment: Alignment.topRight,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width / 2,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  color: mesNotes[i].isFa == 1
+                                      ? colora[mesNotes[i].isFa]
+                                      : colora[0],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      width: double.infinity,
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                        color: mesNotes[i].isFa == 1
+                                            ? colorb[mesNotes[i].isFa]
+                                            : colorb[0],
+                                        //si c'est favoris(1)colorb[1]:colorb[0]
+                                        borderRadius: BorderRadius.vertical(
+                                          top: Radius.circular(10),
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          mesNotes[i].titre.toUpperCase(),
+                                          style: TextStyle(color: Colors.white),
+                                        ),
                                       ),
                                     ),
-                                    child: Center(
+                                    SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Text(
+                                            "${mesNotes[i].j}/${mesNotes[i].m}/${mesNotes[i].y}",
+                                            style: TextStyle(fontSize: 10)),
+                                        Text(
+                                          "${mesNotes[i].heure}h ${mesNotes[i].minute}",
+                                          style: TextStyle(fontSize: 10),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 8),
+                                    Flexible(
+                                      flex: 2,
+                                      child: SingleChildScrollView(
+                                        child: Container(
+                                          padding: EdgeInsets.all(5),
+                                          child: Text(mesNotes[i].note),
+                                        ),
+                                      ),
+                                    ),
+                                    Flexible(
+                                      flex: 0,
                                       child: Text(
-                                        mesNotes[i].titre.toUpperCase(),
-                                        style: TextStyle(color: Colors.white),
+                                        mesNotes[i].name,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
                                       ),
-                                    ),
-                                  ),
-                                  SizedBox(height: 8),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: [
-                                      Text(
-                                          "${mesNotes[i].j}/${mesNotes[i].m}/${mesNotes[i].y}",
-                                          style: TextStyle(fontSize: 10)),
-                                      Text(
-                                        "${mesNotes[i].heure}h ${mesNotes[i].minute}",
-                                        style: TextStyle(fontSize: 10),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(height: 8),
-                                  Flexible(
-                                    flex: 2,
-                                    child: SingleChildScrollView(
+                                    )
+                                  ],
+                                ),
+                              ),
+                              mesNotes[i].isFa == 1
+                                  ? Positioned(
+                                      top: -1,
+                                      right: -1,
                                       child: Container(
-                                        padding: EdgeInsets.all(5),
-                                        child: Text(mesNotes[i].note),
+                                        child: Image.asset("asset/pin.png"),
+                                        width: 20,
+                                        height: 20,
                                       ),
-                                    ),
-                                  ),
-                                  Flexible(
-                                    flex: 0,
-                                    child: Text(
-                                      mesNotes[i].name,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Positioned(
-                              top: -1,
-                              right: -1,
-                              child: Container(
-                                child: Image.asset("asset/pin.png"),
-                                width: 20,
-                                height: 20,
-                              ),
-                            ),
-                          ],
-                        );
-                      },
+                                    )
+                                  : Container(),
+                            ],
+                          );
+                        },
+                      ),
                     );
                   } else {
                     return Center(
@@ -173,14 +214,7 @@ class _MyHommeState extends State<MyHomme> {
           String titre;
           String note;
           String name;
-          List colora = [
-            Color.fromRGBO(100, 12, 5, 0.4),
-            Color.fromRGBO(10, 120, 5, 0.4),
-            Color.fromRGBO(10, 12, 244, 0.4),
-          ];
-          List colorb = [Colors.red, Colors.green, Colors.blue];
-          var r = new Random();
-          var colorChoix = r.nextInt(3);
+
           showDialog(
             context: context,
             builder: (BuildContext context) {
@@ -190,11 +224,10 @@ class _MyHommeState extends State<MyHomme> {
                   vertical: 10,
                 ),
                 children: [
-                  Text("Titre"),
                   TextFormField(
                     decoration: InputDecoration(
                       hintText: "titre de la note",
-                      border: OutlineInputBorder(),
+                      // border: OutlineInputBorder(),
                     ),
                     onChanged: (e) {
                       setState(() {
@@ -202,11 +235,10 @@ class _MyHommeState extends State<MyHomme> {
                       });
                     },
                   ),
-                  Text("Note"),
                   TextFormField(
                     decoration: InputDecoration(
                       hintText: "entrer la note",
-                      border: OutlineInputBorder(),
+                      // border: OutlineInputBorder(),
                     ),
                     onChanged: (n) {
                       setState(() {
@@ -214,11 +246,10 @@ class _MyHommeState extends State<MyHomme> {
                       });
                     },
                   ),
-                  Text("auteur"),
                   TextFormField(
                     decoration: InputDecoration(
                       hintText: "nom de l'auteur",
-                      border: OutlineInputBorder(),
+                      // border: OutlineInputBorder(),
                     ),
                     onChanged: (n) {
                       setState(() {
@@ -227,8 +258,13 @@ class _MyHommeState extends State<MyHomme> {
                     },
                   ),
                   SizedBox(height: 10),
+                  Column(
+                    children: f(),
+                  ),
+                  Text("$gv"),
                   FloatingActionButton(
                     onPressed: () {
+                      print("$gv");
                       print("$titre $note $name");
                       Navigator.pop(context);
                       setState(() {
@@ -236,15 +272,15 @@ class _MyHommeState extends State<MyHomme> {
                           //pour un ajout dans la table note de la bd
                           NotesDataBase.instance.insertNote(
                             MiniCont(
-                              titre,
-                              note,
-                              name,
-                              datetoday().day,
-                              datetoday().month,
-                              datetoday().year,
-                              datetoday().hour,
-                              datetoday().minute,
-                            ),
+                                titre,
+                                note,
+                                name,
+                                datetoday().day,
+                                datetoday().month,
+                                datetoday().year,
+                                datetoday().hour,
+                                datetoday().minute,
+                                0),
                           );
                           // mesNotes.add(
                           //  minicount(...)
@@ -284,8 +320,9 @@ class MiniCont {
   int y;
   int heure;
   int minute;
+  int isFa;
   MiniCont(String titre, String note, String name, int j, int m, int y,
-      int heure, int minute) {
+      int heure, int minute, int isFa) {
     this.titre = titre;
     this.note = note;
     this.name = name;
@@ -296,6 +333,7 @@ class MiniCont {
     this.j = j;
     this.m = m;
     this.y = y;
+    this.isFa = isFa;
   }
 
   Map<String, dynamic> toMap() {
@@ -308,6 +346,7 @@ class MiniCont {
       "annee": y,
       "heure": heure,
       "minute": minute,
+      "isfa": isFa
     };
   }
 
@@ -321,6 +360,7 @@ class MiniCont {
       map["annee"],
       map["heure"],
       map["minute"],
+      map["isfa"],
     );
   }
 }
